99c16ccb9bd4cee9a7d568e7e1eba91bc75a7bef
yeremia dany <dany_yeremia@5star.co.jp>
2019-08-06 18:45:35 +0900

    ie 11　対応

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,15 @@ module.exports = {
   // 出力ファイル
   output: {
     filename: "bundle.js"
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: [{
+          loader: 'babel-loader'
+        }
+      ]
+    }]
   }
 }
